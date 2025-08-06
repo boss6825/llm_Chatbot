9908e5c30473fa5e3f60c628210590db4cb92784
boss6825 <arpitsolanki6825@gmail.com>
2025-08-06 15:23:28 +0000

    agent with search capabilities

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import 'dotenv/config';
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { MemorySaver } from "@langchain/langgraph";
+
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+
+//here i defined tools
+const agentTools = [new TavilySearchResults({ maxResults: 3 })];
+const agentModel = new ChatGoogleGenerativeAI({ model: "gemini-2.0-flash", temperature: 0 });
 
 
-//this i made for logging into langsmith
-// sETUP: Create and configure the client once.
-const llm = new ChatGoogleGenerativeAI({
-    model: "gemini-1.5-flash",
+// initialize memory to persist state between graph runs
+const agentCheckpointer = new MemorySaver();
+const agent = createReactAgent({
+  llm: agentModel,
+  tools: agentTools,
+  checkpointSaver: agentCheckpointer,
 });
 
+// Now it's time to use!
+const agentFinalState = await agent.invoke(
+    { messages: [new HumanMessage("what is the current weather in sf")] },
+    { configurable: { thread_id: "42" } },
+  );
+  
+  console.log(
+    agentFinalState.messages[agentFinalState.messages.length - 1].content,
+  );
+  
+  const agentNextState = await agent.invoke(
+    { messages: [new HumanMessage("what about ny")] },
+    { configurable: { thread_id: "42" } },
+  );
+  
+  console.log(
+    agentNextState.messages[agentNextState.messages.length - 1].content,
+  );
 
 const model = new ChatGoogleGenerativeAI({
     model: "gemini-2.0-flash",
